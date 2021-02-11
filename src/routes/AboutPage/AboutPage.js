88411a6f52bd0e7cf60e78cad16c1051a16c965e
Ian Bruns <ian@rbruns.com>
2021-02-10 19:47:12 -0700

    dummy values for first deploy

--- src/routes/AboutPage/AboutPage.js
+++ src/routes/AboutPage/AboutPage.js
@@ -1,5 +1,40 @@
 import React from 'react'
 
 export default function AboutPage(props) {
-    return <p>AboutPage!</p>
+    return (
+        <>
+            <h2>Hi! I'm Ian, nice to meet you!</h2>
+            <div class="item">
+                <p>
+                    Hello! My name is Ian Bruns, and I am a student developer currently living in Denver, CO.
+                </p>
+                <hr />
+                <p>
+                    I took up programming as a hobby in work from home/quarantine, and found myself absorbed in the
+                    subject
+                    material. I then signed up for the software engineering program through Thinkful! because I want to
+                    make
+                    this a career, not just a hobby. If you want someone who is passionate about the subject material,
+                    you
+                    will find that in me
+                    </p>
+                <hr />
+                <p>
+                    When not programming, you can usually find me spending time with friends and loved ones, working
+                    out,
+                    and honing a ping pong skill that I developed working in payroll for a major corporation. I'm also a
+                    pop
+                    culture trivia library, and can sit down and talk to you about music at all times in the day until
+                    I am
+                    blue in the face
+                    </p>
+                <hr />
+                <p>
+                    So stick around, pull up a chair, and if you like what you see and want to get to know more, please
+                    do
+                    not hesitate to reach out!
+                    </p>
+            </div>
+        </>
+    )
 }
--- src/routes/ContactPage/ContactPage.js
+++ src/routes/ContactPage/ContactPage.js
@@ -1,5 +1,14 @@
 import React from 'react'
+import UnderConstruction from '../../images/UnderConstruction.jpg'
 
 export default function ContactPage(props) {
-    return <p>ContactPage!</p>
+    return (
+        <div className='center'>
+            <img src={UnderConstruction} alt='' height='500px' />
+            <br />
+            <a href='https://www.freepik.com/vectors/background'>
+                Background vector created by starline - www.freepik.com
+            </a>
+        </div>
+    )
 }
