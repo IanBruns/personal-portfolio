8465661ba7ec66bab916488d1feca609bd4c3ead
Ian Bruns <ian@rbruns.com>
2021-02-10 19:04:17 -0700

    trying new Burger Bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,23 +5,6 @@ import './Header.css'
 export default function Header(props) {
     return (
         <div className='header'>
-            <nav role="navigation">
-                <div id="menuToggle">
-
-                    <input type="checkbox" />
-
-                    <span></span>
-                    <span></span>
-                    <span></span>
-
-                    <ul id="menu">
-                        <Link to='/'><li>Home</li></Link>
-                        <Link to='/About'><li>About</li></Link>
-                        <Link to='/Projects'><li>Projects</li></Link>
-                        <Link to='/Contact'><li>Contact</li></Link>
-                    </ul>
-                </div>
-            </nav>
             <h1>Ian Bruns</h1>
         </div>
     )
