852c64d428fbcd8cb5f3f8aac03334da39fab167
Ian Bruns <ian@rbruns.com>
2021-02-12 14:20:48 -0700

    HomePage grid up and organized

--- src/routes/HomePage/HomePage.js
+++ src/routes/HomePage/HomePage.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import HomeIan from '../../images/HomeIan.jpg'
+import './HomePage.css'
 
 export default function HomePage(props) {
     return (
         <div className='HomePage'>
             <h2>Hello, my name is Ian!</h2>
+            <div className='home-container'>
+                <div className="home-photo">
+                    <img src={HomeIan} alt='' height='500px' />
+                </div>
+                <div className="home-about">About</div>
+                <div className="home-projects">Projects</div>
+                <div className="home-contact">Contact</div>
+            </div>
         </div>
     )
 }
