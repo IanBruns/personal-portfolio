78a523f484c821442a5697d1cab385a78db1958a
Ian Bruns <ian@rbruns.com>
2021-03-30 14:54:29 -0600

    added icons to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './Header.css';
+import linkedin from '../../images/icons/contact/LinkedIn.png';
+import github from '../../images/icons/contact/github.png';
 
 export default function Header(props) {
     return (
-        <div className='header'>
+        <React.Fragment>
             <h1>Ian Bruns</h1>
-            <div>
-
+            <div className='header-contact'>
+                <a href='https://www.linkedin.com/in/ianabruns/'>
+                    <img src={linkedin} alt='' height='30px' />
+                </a>
+                <a href='https://github.com/IanBruns'>
+                    <img src={github} alt='' height='30px' />
+                </a>
             </div>
-        </div>
+        </React.Fragment>
     )
 }
