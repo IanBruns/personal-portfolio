fd24f8262c64a6eee103d98726cbbf3237ac6740
Ian Bruns <ian@rbruns.com>
2021-03-30 11:59:03 -0600

    starting over

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
-import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header'
-import AboutPage from './routes/AboutPage/AboutPage';
-import ContactPage from './routes/ContactPage/ContactPage';
-import HomePage from './routes/HomePage/HomePage'
-import NotFoundPage from './routes/NotFoundPage/NotFoundPage';
-import ProjectPage from './routes/ProjectPage/ProjectPage';
 
 function App() {
   return (
@@ -15,25 +9,6 @@ function App() {
       </header>
 
       <main id="page-wrap">
-        <div className='content'>
-          <Switch>
-            <Route exact path='/'
-              component={HomePage}
-            />
-            <Route path='/About'
-              component={AboutPage}
-            />
-            <Route path='/Projects'
-              component={ProjectPage}
-            />
-            <Route path='/Contact'
-              component={ContactPage}
-            />
-            <Route
-              component={NotFoundPage}
-            />
-          </Switch>
-        </div>
       </main>
 
       <footer>
--- src/routes/NotFoundPage/NotFoundPage.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export default function NotFoundPage(props) {
-    return <p>NotFoundPage!</p>
-}
