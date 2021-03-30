c7056037b948e103bc67513336489afe829a3411
Ian Bruns <ian@rbruns.com>
2021-03-30 12:09:28 -0600

    moved Header and App into folders

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { useMediaQuery } from 'react-responsive';
 import './App.css';
-import Header from './components/Header';
+import Header from '../Header/Header';
 
 function App() {
+  const isDesktop = useMediaQuery({ minDeviceWidth: 800 })
+
   return (
     <div className="App">
       <header>
@@ -11,6 +13,12 @@ function App() {
       </header>
 
       <main>
+        {isDesktop && (
+          <p>Desktop</p>
+        )}
+        {!isDesktop && (
+          <p>mobile</p>
+        )}
       </main>
 
       <footer>
---
+++ src/projectStore.js
@@ -0,0 +1,17 @@
+/* Template
+{
+    name: 'App-Name'
+    app-description: 'App-descript'
+    app: 'app-address'
+    repo-client: 'client-repo-address'
+    repo-server: 'server-repo-address'
+    achieved: 'achieved'
+    image: 'app-image'
+}
+*/
+
+const projects = [
+
+]
+
+export default projects;
