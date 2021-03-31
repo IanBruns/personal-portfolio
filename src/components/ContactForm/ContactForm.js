c2d32d65b53e22fea57a2f7f13834de164c3130a
Ian Bruns <ian@rbruns.com>
2021-03-31 15:12:52 -0600

    refactored form into component

---
+++ src/components/ContactForm/ContactForm.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function ContactForm(props) {
+    return (
+        <form action="https://formspree.io/f/mqkglkdg" method="POST">
+            <label for="user-name">Your Name:</label>
+            <input id="user-name" type="text" name="name" required />
+
+            <label for="user-email">Your Email</label>
+            <input id="user-email" type="text" name="_replyto"
+                placeholder="So I can get back to you!" required />
+
+            <label for="user-message">Message:</label>
+            <textarea id="user-message" name="message" placeholder="Put your message here!" />
+            <button type="submit">Send</button>
+            <button type="reset">Clear</button>
+        </form>
+    )
+}
