a384ca6b81dc89f4fb86f252ce20fa2fbf4ed55c
Ian Bruns <ian@rbruns.com>
2021-03-30 19:19:34 -0600

    moved elevator pitch to component

---
+++ src/components/ShortAbout/ShortAbout.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function ShortAbout(props) {
+    return (
+        <React.Fragment>
+            <h2>Hi I'm Ian!</h2>
+            <h3>It's nice to meet you</h3>
+            <p>
+                I'm a full stack developer in the PERN stack currently living in Denver, CO. I'm
+                passionate about using my programming skills to solve problems while contributing to
+                a positive team culture.
+            </p>
+            <p>
+                If you want to work with someone who's passionate about learning and rearching
+                and creating clean, workable code, let's get in touch!
+            </p>
+        </React.Fragment>
+    )
+}
--- src/routes/Intro/Intro.js
+++ src/routes/Intro/Intro.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useMediaQuery } from 'react-responsive';
 import './Intro.css';
 import homePhoto from '../../images/self/HomeIan.jpg'
+import ShortAbout from '../../components/ShortAbout/ShortAbout';
 
 export default function Intro(props) {
     const onDesktop = useMediaQuery({ minDeviceWidth: 800 });
@@ -15,17 +16,7 @@ export default function Intro(props) {
                     </div>
                 )}
                 <div className='right'>
-                    <h2>Hi I'm Ian!</h2>
-                    <h3>It's nice to meet you</h3>
-                    <p>
-                        I'm a full stack developer in the PERN stack currently living in Denver, CO. I'm
-                        passionate about using my programming skills to solve problems while contributing to
-                        a positive team culture.
-                    </p>
-                    <p>
-                        If you want to work with someone who's passionate about learning and rearching
-                        and creating clean, workable code, let's get in touch!
-                    </p>
+                    <ShortAbout />
                 </div>
             </div>
         </div>
