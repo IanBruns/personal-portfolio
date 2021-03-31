34c7a1055cd086911f5795914e53f7d5477690c2
Ian Bruns <ian@rbruns.com>
2021-03-31 15:26:13 -0600

    css and hmltFor

--- src/routes/Contact/Contact.js
+++ src/routes/Contact/Contact.js
@@ -4,6 +4,12 @@ import ContactForm from '../../components/ContactForm/ContactForm';
 export default function Contact(props) {
     return (
         <div className='Contact'>
+            <h2>Let's get in touch!</h2>
+            <p>
+                If you want to get in touch for colalborations/jobs/music recommendations
+                please don't hesitate to reach out!  All of my contact info is in the header,
+                and I also have a form to email me!
+            </p>
             <ContactForm />
         </div>
     )
