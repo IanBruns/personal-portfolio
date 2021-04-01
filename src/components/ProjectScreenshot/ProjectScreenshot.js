4c75a3b5a40cf10bba135ff20213e0c502e61e88
Ian Bruns <ian@rbruns.com>
2021-03-31 20:18:56 -0600

    added screenshots

---
+++ src/components/ProjectScreenshot/ProjectScreenshot.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './ProjectScreenshot.css'
+import routine from '../../images/projectimg/screenshot/RightRoutine.png';
+import oktober from '../../images/projectimg/screenshot/Oktoberfest.png';
+import play from '../../images/projectimg/screenshot/PlayPacket.png';
+
+export default function ProjectScreenshot(props) {
+    let screenshot;
+
+    props.projId === 1 ? screenshot = routine
+        : (props.projId === 2) ? screenshot = oktober
+            : (props.projId === 3) ? screenshot = play
+                : screenshot = null;
+
+    return (
+        <img className='projImg' src={screenshot} alt='' height='400px' width='400px' />
+    )
+}
--- src/components/ProjectTile.js/ProjectTile.js
+++ src/components/ProjectTile.js/ProjectTile.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ProjectIcon from '../ProjectIcon/ProjectIcon';
 import StackIcon from '../StackIcon/StackIcon';
+import ProjectScreenshot from '../ProjectScreenshot/ProjectScreenshot';
 
 export default function ProjectTile(props) {
     return (
@@ -9,20 +10,25 @@ export default function ProjectTile(props) {
                 <ProjectIcon projId={props.project.id} />
                 <h3>{props.project.name}</h3>
             </div>
-            <div>
-                <p>{props.project.description}</p>
-                <StackIcon str={props.project.stack} />
-                <p>{props.project.achieved}</p>
-                <div className='tile-buttons'>
-                    <a href={props.project.liveLink}>
-                        <button>Application</button>
-                    </a>
-                    <a href={props.project.repoClient}>
-                        <button>Client Repository</button>
-                    </a>
-                    <a href={props.project.repoServer}>
-                        <button>Server Repository</button>
-                    </a>
+            <div className='container'>
+                <div className='left'>
+                    <p>{props.project.description}</p>
+                    <StackIcon str={props.project.stack} />
+                    <p>Achieved: {props.project.achieved}</p>
+                    <div className='tile-buttons'>
+                        <a href={props.project.liveLink}>
+                            <button>Application</button>
+                        </a>
+                        <a href={props.project.repoClient}>
+                            <button>Client Repository</button>
+                        </a>
+                        <a href={props.project.repoServer}>
+                            <button>Server Repository</button>
+                        </a>
+                    </div>
+                </div>
+                <div className='right'>
+                    <ProjectScreenshot projId={props.project.id} />
                 </div>
             </div>
         </div>
